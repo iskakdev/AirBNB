f02b62fdb199df9b256ec20e7224de36c5fbef55
iskakdev <eshmatoviskak@gmail.com>
2025-11-25 15:41:21 +0600

    My airbnb_project

--- my_airbnb/my_airbnb/settings.py
+++ my_airbnb/my_airbnb/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'my_airbnb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AirBNB',
+        'USER': 'postgres',
+        'PASSWORD': 'iskak228',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
